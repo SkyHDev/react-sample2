0fd67c176f573118604e26a0d2ef1d96f5487a1b
sergeyhoroshilov <sergeyhoroshilov12@outlook.com>
2021-02-06 09:38:45 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [students, setStudents] = useState([])
+    useEffect(() => {
+        try {
+            axios.get(`https://api.hatchways.io/assessment/students`)
+            .then(res => {
+                console.log(res.data.students)
+                setStudents(res.data.students)
+            })
+        } catch(err) {
+
+        }
+    })
+    return (
+    <div className="container">
+        {}
     </div>
-  );
+    );
 }
 
 export default App;
