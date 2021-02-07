5079e2f77b2119296cd1cf3629782b6086fdb3cd
sergeyhoroshilov <sergeyhoroshilov12@outlook.com>
2021-02-06 19:36:20 -0800

    Api call added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React, { useEffect } from "react";
+import './styles/app.css';
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import Student from './components/Student';
 
 function App() {
     const [students, setStudents] = useState([])
+    const [hasError, setHasError] = useState(false)
+    const [pending, setPending] = useState(true)
+
     useEffect(() => {
+        setHasError(false)
+        setPending(true)
         try {
             axios.get(`https://api.hatchways.io/assessment/students`)
             .then(res => {
-                console.log(res.data.students)
                 setStudents(res.data.students)
             })
+            setPending(false)
         } catch(err) {
-
+            setHasError(true)
+            setPending(false)
         }
-    })
+
+    }, [])
+
     return (
-    <div className="container">
-        {}
-    </div>
+        <div className="d-flex justify-content-center align-items-center vh-100 container">
+            <div id="content" className="p-0 w-75 card">
+                <div className="px-2">
+                    <input placeholder="Search by name" type="text" className="border-bottom form-control" />
+                    <input placeholder="Search by tag" type="text" className="border-bottom form-control" />
+                </div>
+                { hasError && (<div>Error</div>) }
+                { pending && (<div>Loading...</div>) }
+                { !hasError && !pending && students.length === 0 && (<div>No Students</div>) }
+                { 
+                    !hasError && !pending && students.length > 0 && (
+                        students.map((item, index) => (
+                            <Student key={index} data={item} />
+                        ))
+                    )
+                }
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/Student.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import styles from '../styles/student.module.css';
+
+class Student extends Component {
+    constructor(props) {
+        super(props)
+        this.status = { average: 0 };
+    }
+
+    averageGrade = (grades) => {
+        var sum = 0;
+        grades.forEach(element => {
+            sum += parseFloat(element);
+        });
+     
+        let aver = sum / grades.length;
+        return (aver).toFixed(3);
+    }
+
+    render() {
+        return (
+            <div className="border-bottom p-3 media">
+                <img width="128" height="128" src={this.props.data.pic} alt="Personal Img" className="align-self-start border rounded-circle mr-4 mt-3" />
+                <div className="media-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h3 className="mb-0">{this.props.data.firstName} {this.props.data.lastName}</h3>
+                        <h1 className="text-secondary mb-0">+</h1>
+                    </div>
+                    <div className="pl-4">
+                        <p className={styles.privateInfo}>Email: {this.props.data.email}</p>
+                        <p className={styles.privateInfo}>Company: {this.props.data.company}</p>
+                        <p className={styles.privateInfo}>Skill: {this.props.data.skill}</p>
+                        <p className={styles.privateInfo}>Average: {this.averageGrade(this.props.data.grades)}%</p>
+                        <div className="mt-2">
+                            <input placeholder="Add a tag" type="text" className="border-bottom form-control" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    };
+
+}
+
+export default Student;
